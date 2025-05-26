d7a2803bfd70354981030f50d05c8b8f2295f052
Leon <ldcantillom@unimagdalena.edu.co>
2025-05-25 19:44:38 -0500

    [UPDATE] Change vite port to 3000.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,      // Puerto del servidor
+    host: true       // Permite acceso desde IP local (otros dispositivos en la red)
+  }
 });
