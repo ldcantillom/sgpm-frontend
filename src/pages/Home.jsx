35f059645542419b69c1ea54665aa346eb4b3d35
Leon <ldcantillom@unimagdalena.edu.co>
2025-05-27 05:27:55 -0500

    [UPDATE] delete unnecessary message.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,8 +22,6 @@ const Home = () => {
         const data = await response.json();
         setUsuario(data[0]);
         sessionStorage.setItem("usuario", JSON.stringify(data[0]));
-
-        console.log('usuario recibido:', data[0]); 
       } catch (err) {
         setError(err.message);
       } finally {
